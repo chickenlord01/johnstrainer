947c76542987e47520be07f2cccac153276ab1f1
chickenlord01 <johnlapp@live.com>
2026-03-11 14:42:42 -0500

    add convars config file

--- init/server.lua
+++ init/server.lua
@@ -1,2 +1,8 @@
 --Initialise all the needed things and stuff
 if not Trainer then Trainer = {} end
+if GetResourceState("johnsframework") ~= "missing" then Trainer.framework = "johnsframework" end
+
+local convars = require("utils.configuration.convars")
+for k,v in pairs(convars) do
+    ExecuteCommand("setr "..k.. " "..v)
+end
