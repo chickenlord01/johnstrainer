b6f3d2d0587d9ea81d3d67bf2d1fd3bc901e795a
chickenlord01 <johnlapp@live.com>
2026-03-11 12:24:08 -0500

    setup resource

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'John | https://github.com/chickenlord01'
+description 'Lua replacement for vMenu'
+version '0.0.1'
+lua54 'yes'
+
+shared_script '@ox_lib/init.lua'
+
+shared_scripts {
+    'utils/**/shared.lua'
+}
+
+client_scripts {
+    'utils/**/client.lua',
+    'init/client.lua'
+}
+
+server_scripts {
+    'utils/**/server.lua',
+    'init/server.lua'
+}
