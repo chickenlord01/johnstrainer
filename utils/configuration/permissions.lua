5d6e927447ca8c853f07f65371a7e8723b294c28
chickenlord01 <johnlapp@live.com>
2026-03-12 11:25:04 -0500

    add permissions

--- utils/configuration/permissions.lua
+++ utils/configuration/permissions.lua
@@ -1,1 +1,16 @@
-return {}
+--These are the default settings, changes made in game are found elsewhere
+
+--groups type: table(string)
+--status: allow or deny
+return {
+    --Commands
+    ["command.opentrainer"] =              {groups = {"builtin.everyone"}, status = "allow"},
+
+    --Main menu
+    ["johnstrainer.Menu.OnlinePlayers"] =  {groups = {"builtin.everyone"}, status = "allow"},
+    ["johnstrainer.Menu.PlayerRelated"] =  {groups = {"builtin.everyone"}, status = "allow"},
+    ["johnstrainer.Menu.VehicleRelated"] = {groups = {"builtin.everyone"}, status = "allow"},
+    ["johnstrainer.Menu.WorldRelated"] =   {groups = {"builtin.everyone"}, status = "allow"},
+    ["johnstrainer.Menu.Recording"] =      {groups = {"builtin.everyone"}, status = "allow"},
+    ["johnstrainer.Menu.Misc"] =           {groups = {"builtin.everyone"}, status = "allow"},
+}
