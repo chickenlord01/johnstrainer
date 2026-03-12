8cd32a5919f6403e62a64eec5eac8aa59ede3d1e
chickenlord01 <johnlapp@live.com>
2026-03-12 11:26:17 -0500

    init online players menu

--- modules/players/client.lua
+++ modules/players/client.lua
@@ -1,3 +1,56 @@
+while not Trainer.Menu do Wait(0) end
+
 CreateThread(function()
     lib.callback.await("johnstrainer:player:connected",false)
 end)
+
+
+lib.registerMenu({
+    id = 'johnstrainer_online_players',
+    title = 'Online Players',
+    position = 'top-right',
+    onSideScroll = function(selected, scrollIndex, args)
+        --print("Scroll: ", selected, scrollIndex, args)
+    end,
+    onSelected = function(selected, secondary, args)
+        Trainer.Menu.menuPos['johnstrainer_online_players'] = selected
+    end,
+    onCheck = function(selected, checked, args)
+        --print("Check: ", selected, checked, args)
+    end,
+    onClose = function(keyPressed)
+        Trainer.Menu.onClose(false,keyPressed)
+    end,
+    options = {
+        {label = 'nothing here :(', close = false},
+    }
+}, function(selected, scrollIndex, args)
+end)
+
+Trainer.Menu.GenerateOnlinePlayers = function()
+    local players = lib.callback.await("johnstrainer:player:getOnlinePlayers",false)
+    for _,v in pairs(players) do
+        local menuId = "johnstrainer_online_player_"..v.id
+        lib.registerMenu({
+            id = menuId,
+            title = v.name,
+            position = 'top-right',
+            onSideScroll = function(selected, scrollIndex, args)
+                --print("Scroll: ", selected, scrollIndex, args)
+            end,
+            onSelected = function(selected, secondary, args)
+                Trainer.Menu.menuPos[menuId] = selected
+            end,
+            onCheck = function(selected, checked, args)
+                --print("Check: ", selected, checked, args)
+            end,
+            onClose = function(keyPressed)
+                Trainer.Menu.onClose(false,keyPressed)
+            end,
+            options = {
+                {label = 'nothing here :(', close = false},
+            }
+        }, function(selected, scrollIndex, args)
+        end)
+    end
+end
