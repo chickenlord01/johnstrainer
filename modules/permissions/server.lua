e3ef448121c0d06bf83a4670d9dbd3ba3c801710
chickenlord01 <johnlapp@live.com>
2026-03-13 16:05:25 -0500

    init permissions class

--- modules/permissions/server.lua
+++ modules/permissions/server.lua
@@ -1,4 +1,5 @@
 local Permission = lib.class("Permission")
+local defaultPermissions = require("utils.configuration.permissions")
 Trainer.Permissions = {
     PermissionsTable = {}
 }
@@ -11,5 +12,37 @@ function Permission:constructor(data)
     Trainer.Permissions.PermissionsTable[self.id] = self
     return self:setup()
 end
-function Permission:setup()end
+function Permission:setup()
+    return self:applyPermissions()
+end
+function Permission:applyPermissions()
+    for _,v in pairs(self.groups) do
+        lib.addAce(v,self.id,self.status)
+    end
+    return true
+end
+function Permission:removePermissions()
+    for _,v in pairs(self.groups) do
+        lib.removeAce(v,self.id,self.status)
+    end
+    return true
+end
 function Permission:save()end
+
+CreateThread(function()
+    for k,v in pairs(defaultPermissions) do
+        Permission:new({
+            id = k,
+            groups = v.groups,
+            status = v.status
+        })
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        for _,v in pairs(Trainer.Permissions.PermissionsTable) do
+            v:removePermissions()
+        end
+    end
+end)
