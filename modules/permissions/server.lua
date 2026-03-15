cfca28e74ee709412bfe57feb1b41a1e77615897
chickenlord01 <johnlapp@live.com>
2026-03-15 11:19:54 -0500

    add menu options server callback for permissions

--- modules/permissions/server.lua
+++ modules/permissions/server.lua
@@ -46,3 +46,46 @@ AddEventHandler('onResourceStop', function(resource)
         end
     end
 end)
+
+lib.callback.register('johnstrainer:permissions:getMenuOptionPermissions', function(source,menu)
+    local tempTable = {}
+    if menu == "main" then
+        if IsPlayerAceAllowed(source,"johnstrainer.Menu.OnlinePlayers") then
+            table.insert(tempTable,{
+                label = "Online players",
+                args = {"johnstrainer_online_players"}
+            })
+        end
+        if IsPlayerAceAllowed(source,"johnstrainer.Menu.PlayerRelated") then
+            table.insert(tempTable,{
+                label = "Player related options",
+                args = {"johnstrainer_player_options"}
+            })
+        end
+        if IsPlayerAceAllowed(source,"johnstrainer.Menu.VehicleRelated") then
+            table.insert(tempTable,{
+                label = "Vehicle related options",
+                args = {"johnstrainer_vehicle_options"}
+            })
+        end
+        if IsPlayerAceAllowed(source,"johnstrainer.Menu.WorldRelated") then
+            table.insert(tempTable,{
+                label = "World related options",
+                args = {"johnstrainer_world_options"}
+            })
+        end
+        if IsPlayerAceAllowed(source,"johnstrainer.Menu.Recording") then
+            table.insert(tempTable,{
+                label = "Recording options",
+                args = {"johnstrainer_recording_options"}
+            })
+        end
+        if IsPlayerAceAllowed(source,"johnstrainer.Menu.Misc") then
+            table.insert(tempTable,{
+                label = "Misc options",
+                args = {"johnstrainer_misc_options"}
+            })
+        end
+    end
+    return tempTable
+end)
