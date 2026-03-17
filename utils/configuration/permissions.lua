0176184bad35ec8e909511dbd29a4adeda28b0cb
chickenlord01 <johnlapp@live.com>
2026-03-17 15:13:52 -0500

    add player related and player options permissions

--- utils/configuration/permissions.lua
+++ utils/configuration/permissions.lua
@@ -26,4 +26,32 @@ return {
     ["johnstrainer.OnlinePlayers.Unban"] =        {groups = {"builtin.everyone"}, status = true},
     ["johnstrainer.OnlinePlayers.Identifiers"] =  {groups = {"builtin.everyone"}, status = true},
 
+    --Player Related
+    ["johnstrainer.PlayerRelated.PlayerOptions"] =     {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerRelated.PlayerAppearance"] =  {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerRelated.PlayerCharacters"] =  {groups = {"builtin.everyone"}, status = true},
+
+    --Player Options
+    ["johnstrainer.PlayerOptions.God"] =                   {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.Invisible"] =             {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.UnlimitedStamina"] =      {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.FastRun"] =               {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.FastSwim"] =              {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.Superjump"] =             {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.NoRagdoll"] =             {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.NeverWanted"] =           {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.SetWanted"] =             {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.ClearBlood"] =            {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.SetBlood"] =              {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.Ignored"] =               {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.StayInVehicle"] =         {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.MaxHealth"] =             {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.MaxArmor"] =              {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.CleanPlayer"] =           {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.DryPlayer"] =             {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.WetPlayer"] =             {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.VehicleAutoPilotMenu"] =  {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.Freeze"] =                {groups = {"builtin.everyone"}, status = true},
+    ["johnstrainer.PlayerOptions.Scenarios"] =             {groups = {"builtin.everyone"}, status = true},
+
 }
