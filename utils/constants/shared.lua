855a3ce86f862ead8531d71587c2ff70cbaf2afc
chickenlord01 <johnlapp@live.com>
2026-04-18 14:53:46 -0500

    add ped appearance constants

--- utils/constants/shared.lua
+++ utils/constants/shared.lua
@@ -3,3 +3,10 @@ string = lib.string
 
 --Initializes ox_lib locales
 lib.locale()
+
+--Initializes constants global table
+constants = {}
+
+--Ped appearance based constants
+constants.pedComponents = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+constants.pedProps = {0, 1, 2, 6, 7}
