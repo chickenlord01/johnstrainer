2a27553cc76521619f5ca77d371ed978a61819cc
chickenlord01 <johnlapp@live.com>
2026-04-18 22:55:21 -0500

    add tattoo zone constant

--- utils/constants/shared.lua
+++ utils/constants/shared.lua
@@ -10,3 +10,13 @@ constants = {}
 --Ped appearance based constants
 constants.pedComponents = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 constants.pedProps = {0, 1, 2, 6, 7}
+constants.pedTattooZones = {
+    ZONE_TORSO = 0,
+	ZONE_HEAD = 1,
+	ZONE_LEFT_ARM = 2,
+	ZONE_RIGHT_ARM = 3,
+	ZONE_LEFT_LEG = 4,
+	ZONE_RIGHT_LEG = 5,
+	ZONE_UNKNOWN = 6,
+	ZONE_NONE = 7
+}
