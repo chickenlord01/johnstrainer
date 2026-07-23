acc44c5a4c6ef8d9d7816725b959f476df34ca10
chickenlord01 <johnlapp@live.com>
2026-07-22 20:42:39 -0500

    pedSpawnerMenu

---
+++ modules/menu/pedSpawnerMenu.lua
@@ -0,0 +1,49 @@
+local Menu = require("modules.menu.menuObject")
+
+constants.peds = lib.callback.await("johnstrainer:ped:getPedList",false)
+constants.pedCategories = {}
+
+for _,v in pairs(constants.peds) do
+    if not lib.table.contains(constants.pedCategories,v.type) then
+        table.insert(constants.pedCategories,v.type)
+    end
+end
+
+local subMenus = require("modules.menu.pedSpawnerSubMenu")
+
+local SpawnPedMenu = Menu:new("johnstrainer:spawnped","Spawn Ped",{
+    parent = "johnstrainer:playerappearance",
+    position = 'top-right',
+    onPressed = function(selected, scrollIndex, args)
+        
+    end,
+    onShow = function(args)
+        generatePedSpawnOptions()
+    end
+})
+
+local function generatePedDataTable()
+    local tempData = {}
+    for k,v in pairs(constants.peds) do
+        if not tempData[v.type] then
+            tempData[v.type] = {}
+        end
+        table.insert(tempData[v.type],v)
+    end
+    return tempData
+end
+
+function generatePedSpawnOptions()
+    local options = {}
+    local categories = generatePedDataTable()
+    for category,pedData in pairs(categories) do
+        table.insert(options,{
+            label = category,
+            args = {category = category,peds = pedData}
+        })
+    end
+    SpawnPedMenu:setOptions(options)
+    return options
+end
+
+return SpawnPedMenu
