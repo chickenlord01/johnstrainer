2135bf61dccc05b8da70f91ff0cda911c429a240
chickenlord01 <johnlapp@live.com>
2026-07-23 19:12:34 -0500

    add custom ped categories

---
+++ utils/configuration/shared.lua
@@ -0,0 +1,14 @@
+return {
+    pedCategories = {
+        ["Freemode"] = {
+            {
+                model = "m_f_freemode_01",
+                label = "Female",
+            },
+            {
+                model = "m_m_freemode_01",
+                label = "Male",
+            }
+        }
+    }
+}
